27da9362f197dff3e87b2a6d10eec781691acb3d
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-28 22:07:41 +0000

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,21 @@ const NavBar = () => {
   return (
     <div className="navbar">
       {/*{code here}*/}
+<<<<<<< HEAD
       <NavLink to="/" exact activeStyle={{background: 'darkblue'}}>Home</NavLink>
       <NavLink to="/movies" exact activeStyle={{background: 'darkblue'}}>Movies</NavLink>
       <NavLink to="/directors" exact activeStyle={{background: 'darkblue'}}>Directors</NavLink>
       <NavLink to="/actors" exact activeStyle={{background: 'darkblue'}}>Actors</NavLink>
+=======
+      <NavLink to="/"  activeStyle={{background: 'darkblue'}}>Home</NavLink>
+      <NavLink to="/movies" activeStyle={{background: 'darkblue'}}>Movies</NavLink>
+      <NavLink to="/directors" activeStyle={{background: 'darkblue'}}>Directors</NavLink>
+      <NavLink to="actors" activeStyle={{background: 'darkblue'}}>Actors</NavLink>
+      <NavLink to="/" activeStyle={{background: 'darkblue'}}/>
+      <NavLink to="/movies" activeStyle={{background: 'darkblue'}}/>
+      <NavLink to="/directors" activeStyle={{background: 'darkblue'}}/>
+      <NavLink to="actors" activeStyle={{background: 'darkblue'}}/>
+>>>>>>> a7bfb9228ec579bce2861166efdd534bc135acb5
     </div>
   );
 };
