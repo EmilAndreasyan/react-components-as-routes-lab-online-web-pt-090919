d79d89d991f72e22cb65235085084f908dd57d4a
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-08-06 04:04:50 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,12 +5,10 @@ const NavBar = () => {
   return (
     <div className="navbar">
       {/*{code here}*/}
-<<<<<<< HEAD
       <NavLink to="/" exact activeStyle={{background: 'darkblue'}}>Home</NavLink>
       <NavLink to="/movies" exact activeStyle={{background: 'darkblue'}}>Movies</NavLink>
       <NavLink to="/directors" exact activeStyle={{background: 'darkblue'}}>Directors</NavLink>
       <NavLink to="/actors" exact activeStyle={{background: 'darkblue'}}>Actors</NavLink>
-=======
       <NavLink to="/"  activeStyle={{background: 'darkblue'}}>Home</NavLink>
       <NavLink to="/movies" activeStyle={{background: 'darkblue'}}>Movies</NavLink>
       <NavLink to="/directors" activeStyle={{background: 'darkblue'}}>Directors</NavLink>
@@ -19,7 +17,6 @@ const NavBar = () => {
       <NavLink to="/movies" activeStyle={{background: 'darkblue'}}/>
       <NavLink to="/directors" activeStyle={{background: 'darkblue'}}/>
       <NavLink to="actors" activeStyle={{background: 'darkblue'}}/>
->>>>>>> a7bfb9228ec579bce2861166efdd534bc135acb5
     </div>
   );
 };
